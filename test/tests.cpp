39a43f53c581335bfb98b71da86f298edd8a1ae9
katione <147106388+katione@users.noreply.github.com>
2024-05-03 17:16:28 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,13 +1,13 @@
 // Copyright 2024 katione
-#include "pch.h"
 #include <gtest/gtest.h>
 #include <string>
-#include "../labba6_mips/textgen.h"
-using namespace std;
+#include "../include/textgen.h"
+#include "pch.h"
+
 
 TEST(TextGenTest, test1) {
     Text_Gen textgen;
-    string filestring = "This is test";
+    std::string filestring = "This is test";
     textgen.text_file(2, filestring);
     Text_Gen::prefix pref = { "This", "is" };
 
@@ -16,7 +16,7 @@ TEST(TextGenTest, test1) {
 
 TEST(TesxtGenTest, test2) {
     Text_Gen textgen;
-    string filestring = "This is test";
+    std::string filestring = "This is test";
     textgen.text_file(2, filestring);
     Text_Gen::prefix pref = { "This", "is" };
     EXPECT_EQ(textgen.statetab[pref][0], "test");
@@ -24,31 +24,33 @@ TEST(TesxtGenTest, test2) {
 
 TEST(TextGenTest, test3) {
     Text_Gen textgen;
-    string filestring = "This is a test";
+    std::string filestring = "This is a test";
     textgen.text_file(1, filestring);
-    string res = textgen.text_generation(3, time(0));
-    bool check = (res == "This is a ") || (res == "is a test ") || (res == "a test ") || (res == "test ");
+    std::string res = textgen.text_generation(3, time(0));
+    bool check = (res == "This is a ") || 
+    (res == "is a test ") || (res == "a test ")
+    || (res == "test ");
     ASSERT_TRUE(check);
 }
 
 TEST(TextGenTest, test4) {
     Text_Gen textgen;
-    string filestring = "an apple an orange";
+    std::string filestring = "an apple an orange";
     textgen.text_file(1, filestring);
     Text_Gen::prefix pref = { "an" };
-    vector<string> suf = { "apple", "orange" };
+    std::vector<std::string> suf = { "apple", "orange" };
     EXPECT_EQ(textgen.statetab[pref], suf);
 }
 
 TEST(TextGenTest, test5) {
     Text_Gen textgen;
-    typedef deque<string> prefix;
-    map<prefix, vector<string>> state = {
+    typedef std::deque<std::string> prefix;
+    std::map<prefix, std::vector<std::string>> state = {
         { { "I" }, { "want" } },
         { { "want" }, { "eat", "sleep" } },
         { {"eat"}, {"apple"} }
     };
     textgen.statetab = state;
-    string res = textgen.text_generation(10, 20);
+    std::string res = textgen.text_generation(10, 20);
     EXPECT_EQ("", res);
 }
