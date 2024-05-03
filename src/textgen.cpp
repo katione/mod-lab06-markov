170b736f44c86eb420b7ba6a30e03f7948d227c0
katione <147106388+katione@users.noreply.github.com>
2024-05-03 17:05:29 +0300

    Update textgen.cpp

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -8,12 +8,12 @@
 #include <ostream>
 #include "textgen.h"
 
-using namespace std;
 
-void Text_Gen::text_file(int NPREF, string& filestring) {
-	string word;
-	istringstream input_text(filestring);
-	while (input_text >> word) {
+
+void Text_Gen::text_file(int NPREF, const std::string& filestring) {
+    std::string word;
+    std::istringstream input_text(filestring);
+    while (input_text >> word) {
         if (p_prefix.size() < NPREF) {
             p_prefix.push_back(word);
             p_refix = p_prefix;
@@ -22,14 +22,15 @@ void Text_Gen::text_file(int NPREF, string& filestring) {
             p_prefix.pop_front();
             p_prefix.push_back(word);
         }
-	}
+    }
 }
 
-string Text_Gen::text_generation(int MAXGEN_lenght , int ti_me) {
+std::string Text_Gen::text_generation(int MAXGEN_lenght , int ti_me) {
     srand((unsigned int)time(NULL));
     prefix pp_prefix = p_refix;
-    string result= " ";
+    std::string result = " ";
     int random;
+    unsigned int random_time = time(NULL);
     for (auto i = pp_prefix.begin(); i != pp_prefix.end(); i++) {
         result += *i + " ";
     }
@@ -38,8 +39,8 @@ string Text_Gen::text_generation(int MAXGEN_lenght , int ti_me) {
         if (statetab[pp_prefix].empty()) {
             break;
         }
-        random = rand() % statetab[pp_prefix].size();
-        string suff = statetab[pp_prefix][random];
+        random = rand_r(&random_time) % statetab[pp_prefix].size();
+        std::string suff = statetab[pp_prefix][random];
         result += suff + " ";
         pp_prefix.pop_front();
         pp_prefix.push_back(suff);
